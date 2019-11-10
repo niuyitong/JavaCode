4d01bcd7aca8fd884c6f55708a32b2c8a4e68f11
niuyitong <913451387@qq.com>
2019-11-10 17:42:59 +0800

    更新链表 回文 删除重复 判断有环 找入环节点 分割

--- 11_3/src/MySingleList.java
+++ 11_3/src/MySingleList.java
@@ -18,11 +18,11 @@ class MySingleList {
     }
 
     //头插法
-    public void addFirst(int data){
+    public void addFirst(int data) {
         ListNode node = new ListNode(data);
-        if(this.head == null) {//无节点时
+        if (this.head == null) {//无节点时
             this.head = node;
-        }else {
+        } else {
             node.next = this.head;
             this.head = node;
         }
@@ -33,9 +33,9 @@ class MySingleList {
         ListNode node = new ListNode(data);
         ListNode cur = this.head;
         //0、判断是否是第一次插入
-        if(this.head == null) {
+        if (this.head == null) {
             this.head = node;
-        }else {
+        } else {
             //1、找尾巴
             while (cur.next != null) {
                 cur = cur.next;
@@ -46,16 +46,19 @@ class MySingleList {
     }
 
     //插入到index位置
+
     /**
      * 找到index-1位置的节点  返回当前节点的引用
+     *
      * @param index
      * @return
      */
+
     private ListNode searchIndex(int index) {
         //prev-->index-1;
         ListNode prev = this.head;
         int count = 0;
-        while (count < index-1) {//找到index的前一个节点，从头开始，需要找index-1次
+        while (count < index - 1) {//找到index的前一个节点，从头开始，需要找index-1次
             prev = prev.next;
             count++;
         }
@@ -63,13 +66,13 @@ class MySingleList {
     }
 
     //任意位置插入,第一个数据节点为0号下标
-    public boolean addIndex(int index,int data){
+    public boolean addIndex(int index, int data) {
         //下标不合法
-        if(index < 0 || index > getLength()) {
+        if (index < 0 || index > getLength()) {
             return false;
         }
         //头插法
-        if(index == 0) {
+        if (index == 0) {
             addFirst(data);
             return true;
         }
@@ -91,23 +94,23 @@ class MySingleList {
     }
 
     //打印单链表数据
-    public void display(){
-        if(this.head == null) {
+    public void display() {
+        if (this.head == null) {
             return;
         }
         ListNode cur = this.head;
         while (cur != null) {
-            System.out.print(cur.data+" ");
+            System.out.print(cur.data + " ");
             cur = cur.next;
         }
         System.out.println();
     }
 
     //查找是否包含关键字key是否在单链表中
-    public ListNode contains (int key){
+    public ListNode contains(int key) {
         ListNode cur = this.head;
-        while(cur!=null){
-            if(cur.data==key){
+        while (cur != null) {
+            if (cur.data == key) {
                 return cur;
             }
             cur = cur.next;
@@ -118,8 +121,8 @@ class MySingleList {
     //删除第一次出现关键字为key的节点
     private ListNode searchPrev(int key) {
         ListNode prev = this.head;
-        while(prev.next!=null){
-            if(prev.next.data==key){
+        while (prev.next != null) {
+            if (prev.next.data == key) {
                 return prev;
             }
             prev = prev.next;
@@ -127,48 +130,48 @@ class MySingleList {
         return null;
     }
 
-        public void remove(int key){
-            //1、删除的节点如果是头结点
-            if(this.head.data == key) {
-                this.head = this.head.next;
-                return;
-            }
-            //2、找到删除的节点的前驱  如果找不到  返回null
-            ListNode prev = searchPrev(key);
-            if(prev == null) {
-                System.out.println("没有你要删除的节点");
-                return;
-            }
-            ListNode del = prev.next;
-            //3、进行删除
-            prev.next = del.next;
+    public void remove(int key) {
+        //1、删除的节点如果是头结点
+        if (this.head.data == key) {
+            this.head = this.head.next;
+            return;
         }
+        //2、找到删除的节点的前驱  如果找不到  返回null
+        ListNode prev = searchPrev(key);
+        if (prev == null) {
+            System.out.println("没有你要删除的节点");
+            return;
+        }
+        ListNode del = prev.next;
+        //3、进行删除
+        prev.next = del.next;
+    }
 
     //删除所有关键字为key的节点
-    public void removeAllKey(int key){
+    public void removeAllKey(int key) {
         ListNode prev = this.head;
         ListNode cur = this.head.next;//当前要删除的节点
-        while(cur != null){
-            if(prev.next.data==key){
+        while (cur != null) {
+            if (prev.next.data == key) {
                 prev.next = cur.next; //删除
                 cur = cur.next;
-            }else {
+            } else {
                 cur = cur.next;
                 prev = prev.next;
             }
         }
         //最后处理头节点
-        if(this.head.data == key){
+        if (this.head.data == key) {
             this.head = this.head.next;
         }
     }
 
     //清空节点
     //java自动回收 前提：什么时候不用这块内存
-    public void clear(){
+    public void clear() {
         //this.head = null;//暴力
         //一个一个删
-        while(this.head.next!=null){
+        while (this.head.next != null) {
             ListNode cur = this.head.next;
             this.head.next = cur.next;
         }
@@ -176,17 +179,17 @@ class MySingleList {
     }
 
 
-
     //反转单链表
-    public ListNode reverseList(){
+    public ListNode reverseList() {
         ListNode prev = null;
         ListNode cur = this.head;
         ListNode newHead = null;
-        while(cur!=null){
+        while (cur != null) {
             ListNode curNext = cur.next;
-            if(curNext == null){
+            if (curNext == null) {
                 newHead = cur;
             }
+            //反转
             cur.next = prev;
             prev = cur;
             cur = curNext;
@@ -194,23 +197,23 @@ class MySingleList {
         return newHead;
     }
 
-    public void display2(ListNode head){
-        if(head == null) {
+    public void display2(ListNode head) {
+        if (head == null) {
             return;
         }
         ListNode cur = head;
         while (cur != null) {
-            System.out.print(cur.data+" ");
+            System.out.print(cur.data + " ");
             cur = cur.next;
         }
         System.out.println();
     }
 
     //链表的中间节点
-    public ListNode middleNode1(){//遍历两遍
-        int len = getLength()/2;
+    public ListNode middleNode1() {//遍历两遍
+        int len = getLength() / 2;
         ListNode cur = this.head;
-        while(len!=0){
+        while (len != 0) {
             cur = cur.next;
             len--;
         }
@@ -226,29 +229,177 @@ class MySingleList {
         }
         return slow;
     }
+
     //倒数第K个节点
-    public ListNode findKthToTail(int k){
-        if(k<=0 || head == null){
+    public ListNode findKthToTail(int k) {
+        if (k <= 0) {
             return null;
         }
         ListNode fast = this.head;
         ListNode slow = this.head;
-        while(k-1!=0){
-            if(fast.next != null){
+        while (k - 1 != 0) {
+            if (fast.next != null) {
                 fast = fast.next;
                 k--;
-            } else{
+            } else {
                 System.out.println("没有这个节点");
                 return null;
             }
         }
-        while(fast.next != null){
+        while (fast.next != null) {
             fast = fast.next;
             slow = slow.next;
         }
         return slow;
     }
 
+
+    //以x为基准分割链表,小于X的在左边，大于的在右边
+    //1 3 8 4 9  ==>  若x为5  1 3 4 8 9
+    public ListNode partition(int x) {
+        ListNode bs = null;
+        ListNode be = null;
+        ListNode as = null;
+        ListNode ae = null;
+        ListNode cur = this.head;
+        while (cur != null) {//遍历数组
+            if (cur.data < x) {
+                //是不是第一次插入
+                if (bs == null) {
+                    bs = cur;
+                    be = cur;
+
+                } else {
+                    be.next = cur;
+                    be = cur;
+                }
+            } else {
+                //是不是第一次插入
+                if (as == null) {
+                    as = cur;
+                    ae = cur;
+
+                } else {
+                    ae.next = cur;
+                    ae = cur;
+                }
+            }
+            cur = cur.next;
+        }
+        //如果第一个区间没有数据
+        if (bs == null) {
+            return as;
+        }
+        be.next = as;//将两部分链接起来
+        if(as != null){//避免死循环
+            ae.next = null;
+        }
+        return bs;
+    }
+
+    //有序的链表中，删除重复的节点
+    //原：1 2 3 3 4 4 5  后：1 2 5
+    public ListNode deleteDuplication(){
+        ListNode cur = this.head;
+        ListNode newHead = new ListNode(-1);//虚拟头节点
+        ListNode tmp = newHead;
+        while (cur != null) {//遍历一遍
+            //重复的节点
+            if(cur.next != null
+                    && cur.data == cur.next.data) {
+                while(cur.next != null//这个数重复几次就循环几遍
+                        && cur.data == cur.next.data){
+                    cur = cur.next;
+                }
+                cur = cur.next;
+            }else {
+                tmp.next = cur;
+                tmp = tmp.next;
+                cur = cur.next;
+            }
+        }
+        tmp.next = null;//如果最后一个节点也为重复的节点，将tmp.next置为空
+        return newHead.next;
+    }
+
+
+    //判断是否是回文链表
+    public boolean chkPalindrome() {
+        //1、找到单链表的中间节点
+        ListNode fast = this.head;
+        ListNode slow = this.head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        //2、反转单链表
+        ListNode cur = slow.next;
+        while (cur != null) {
+            ListNode curNext = cur.next;
+            cur.next = slow;
+            slow = cur;
+            cur = curNext;
+        }
+        //3、fast/slow往前    head往后走
+        while(this.head != slow){
+            if(this.head.data != slow.data){
+                return false;
+            }
+            if(this.head.next == slow){//偶数节点返回
+                return true;
+            }
+            slow = slow.next;
+            this.head = this.head.next;
+        }
+        return true;//奇数节点返回
+    }
+
+    //创造一个带环的链表
+    public void creteLoop() {
+        ListNode cur = this.head;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = this.head.next.next;
+    }
+    //判断单链表是否有环
+    public boolean hasCycle(){
+    //只要相遇，就有环
+        ListNode slow = this.head;
+        ListNode fast = this.head;
+        while(fast != null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){//相遇了
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //找到环的入口
+    //fast一次走两步，slow一次走一步，两者相遇后，将fast/slow置为头，
+    // 接下来两者每次走一步，相遇的节点就是环的入口
+    public ListNode detectCycle(){
+        ListNode slow = this.head;
+        ListNode fast = this.head;
+        while(fast != null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){//相遇了，有环
+                break;
+            }
+        }
+        if(fast == null || fast.next == null){
+            return null;//无环
+        }
+        slow = this.head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
 
 
 
--- 11_3/src/TestDemo.java
+++ 11_3/src/TestDemo.java
@@ -1,12 +1,15 @@
 public class TestDemo {
     public static void main(String[] args) {
         MySingleList mySingleList = new MySingleList();
+        /*
         mySingleList.addIndex(0,199);
         mySingleList.addLast(2);
         mySingleList.addLast(2);
         mySingleList.addLast(3);
         mySingleList.addLast(2);
         mySingleList.addLast(5);
+        mySingleList.addLast(6);
+        mySingleList.addLast(3);
         mySingleList.display();
         mySingleList.addIndex(0,2);
         mySingleList.display();
@@ -22,21 +25,51 @@ public class TestDemo {
         mySingleList.remove(199);
         System.out.println("删除199");
         mySingleList.display();
+        */
+        mySingleList.addLast(1);
+        mySingleList.addLast(2);
+        mySingleList.addLast(3);
+        mySingleList.addLast(4);
+        mySingleList.addLast(5);
+        mySingleList.addLast(4);
+        mySingleList.addLast(3);
+        mySingleList.addLast(2);
+        mySingleList.addLast(1);
+        mySingleList.display();
 
+        //创建一个有环链表
+        mySingleList.creteLoop();
+        //判断链表是否有环
+        boolean a = mySingleList.hasCycle();
+        System.out.println(a);
+        //找环的入口
+        ListNode c = mySingleList.detectCycle();
+        System.out.println(c.data);
+        /*
+        //判断是否是回文链表
+        boolean b = mySingleList.chkPalindrome();
+        System.out.println(b);
+        mySingleList.display();//1 2 2 3 4 4 5 6 6
+        //有序的链表删除重复的节点
+        ListNode newHead = mySingleList.deleteDuplication();
+        mySingleList.display2(newHead);//1 3 5
+        //以x为基准分割链表
+        //ListNode node = mySingleList.partition(10);
+        //mySingleList.display2(node);
         //逆置
        // ListNode newHead = mySingleList.reverseList();
        // mySingleList.display2(newHead);
 
         //中间节点
-        ListNode center = mySingleList.middleNode2();
-        System.out.println(center.data);
+        //ListNode center = mySingleList.middleNode2();
+        //System.out.println(center.data);
 
-        //
-        ListNode k = mySingleList.findKthToTail(4);
-        System.out.println(k.data);
+        //倒数第k个节点
+        //ListNode k = mySingleList.findKthToTail(4);
+        //System.out.println(k.data);
         //mySingleList.clear();
         //System.out.println("fakfalls");
-
+*/
        /* boolean flg = mySingleList.contains(99);
         System.out.println(flg);
         ListNode node = mySingleList.contains(500);
