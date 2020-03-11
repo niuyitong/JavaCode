19fc9e4642addffb10f6609e795ae5b3582a779d
niuyitong <913451387@qq.com>
2020-03-11 17:17:09 +0800

    牛客day07

--- 2020_03_09/src/TestDemo.java
+++ 2020_03_09/src/TestDemo.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
-class Test{
+
+public class TestDemo {
     //不要二
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt(), n = sc.nextInt();
         int evenICount = (n / 4) * 2 + (n % 4 < 2 ? n % 4 : 2);
@@ -14,6 +15,30 @@ class Test{
         System.out.println(ans);
     }
     //求最小公倍数
+    //法二(a*b==a和b的最大公约数乘以a和b的最小公倍数
+    public static int lcm(int m, int n) {
+        return (m * n) / gcd(m, n);
+    }
+    //求最大公约数
+    private static int gcd(int a, int b) {
+        int r = a%b;
+        while(r!=0){
+            a = b;
+            b = r;
+            r = a%b;
+        }
+        return b;
+        /*
+        if (b == 0) return a;
+        return gcd(b, a % b);*/
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(lcm(a, b));
+    }
+    //法一
     public static void main1(String[] args) {
         Scanner in = new Scanner(System.in);
         int A = in.nextInt();
@@ -26,8 +51,5 @@ class Test{
         }
         System.out.println(min);
     }
-}
-public class TestDemo {
-
 }
 
