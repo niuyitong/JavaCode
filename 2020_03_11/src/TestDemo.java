4b56c15ba3fe4c1a21bda96b93bf2b00051f327d
niuyitong <913451387@qq.com>
2020-03-13 14:52:22 +0800

    牛客day09

--- 2020_03_11/src/TestDemo.java
+++ 2020_03_11/src/TestDemo.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class TestDemo {
     //将数字以字符串的形式倒序输出
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String str = String.valueOf(n);
@@ -12,30 +12,17 @@ public class TestDemo {
         }
     }
     //末尾0的个数
-    public static int fib(int n){
-        int sum = 1;
-        while(n!=0){
-            sum *= n;
-            n--;
-        }
-        return sum;
-    }
-    public static void main1(String[] args){
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNextInt()){
-            int n = sc.nextInt();
-            int ret = fib(n);
-            int count = 0;
-            while(ret!=0){
-                int tmp = ret%10;
-                if(tmp==0){
-                    count++;
-                }else{
-                    break;
-                }
-                ret /= 10;
-            }
-            System.out.println(count);
-        }
-    }
+    public static void main(String[] args) { 
+		Scanner sc = new Scanner(System.in); 
+		int n = sc.nextInt(); 
+		int res = 0; 
+		for(int i = n; i >= 5; i--) { 
+			int tmp = i; 
+			while(tmp % 5 == 0) { 
+				res++; 
+				tmp /= 5; 
+			} 
+		}
+		System.out.println(res); 
+	}
 }
